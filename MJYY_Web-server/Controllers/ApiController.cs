1552141e7869e87ddddb31c507e7146eb5256b24
Hgnim <hgn1m@outlook.com>
2026-02-12 19:01:59 +0800

    为网关控制器添加更多功能；新增api控制器

---
+++ MJYY_Web-server/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MJYY_Web_server.Controllers {
+	[Route("[controller]")]
+	[ApiController]
+	public class ApiController : ControllerBase {
+		[Route("check")]
+		[HttpGet]
+		public IActionResult Check() => Ok();
+	}
+}
--- MJYY_Web-server/Controllers/GatewayController.cs
+++ MJYY_Web-server/Controllers/GatewayController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MJYY_Web_server.Middleware;
 
 namespace MJYY_Web_server.Controllers {
 	[Route("[controller]")]
@@ -6,12 +7,17 @@ namespace MJYY_Web_server.Controllers {
 	public class GatewayController : ControllerBase {
 
 		private readonly IConfiguration _config;
+		private readonly ILogger<ApiKeyMiddleware> _logger;
 
-		public GatewayController(IConfiguration config) {
+		public GatewayController(
+			IConfiguration config,
+			ILogger<ApiKeyMiddleware> logger
+			) {
 			_config = config;
+			_logger = logger;
 
 			//将配置文件中的网关配置载入内存字典
-			Gateways= ((Func<Dictionary<string, GatewayInfo>>)(() => {
+			Gateways = ((Func<Dictionary<string, GatewayInfo>>)(() => {
 				Dictionary<string, GatewayInfo> dict = new(StringComparer.OrdinalIgnoreCase);
 				GatewayInfo[]? read = _config.GetSection("Config:Gateway:Target").Get<GatewayInfo[]>();
 				if (read != null) {
@@ -24,27 +30,94 @@ namespace MJYY_Web_server.Controllers {
 		}
 
 		public class GatewayInfo {
+			/// <summary>
+			/// 目标网关名称
+			/// </summary>
 			public required string Name { get; set; }
+			/// <summary>
+			/// 目标地址
+			/// </summary>
 			public required string Url { get; set; }
 			public enum ResultTypeEnum {
 				json,
 				iframe
 			}
+			/// <summary>
+			/// 返回类型
+			/// </summary>
 			public ResultTypeEnum ResultType { get; set; } = ResultTypeEnum.json;
+			public class PasswordClass {
+				/// <summary>
+				/// 是否启用密码验证
+				/// </summary>
+				public required bool Enable { get; set; }
+				/// <summary>
+				/// 密码
+				/// </summary>
+				public required string Value { get; set; }
+			}
+			/// <summary>
+			/// 密码验证配置
+			/// </summary>
+			public PasswordClass Password { get; set; } = new() {
+				Enable = false,
+				Value = ""
+			};
+			public class Parameter {
+				/// <summary>
+				/// 参数名
+				/// </summary>
+				public required string Name { get; set; }
+				/// <summary>
+				/// 参数值
+				/// </summary>
+				public required object Value { get; set; }
+			}
+			/// <summary>
+			/// 返回json时附加的参数，仅json模式时生效
+			/// </summary>
+			public Parameter[] Parameters { get; set; } = [];
+		}
+		public class GatewayPostBody {
+			public string? Password { get; set; } = null;
 		}
 
 		private readonly Dictionary<string, GatewayInfo> Gateways;
 
 		[Route("{gatewayName}/{**path}")]
 		[HttpGet]
-		public IActionResult Gateway(string gatewayName, string? path) {
+		[HttpPost]
+		[Consumes("application/json")]
+		public IActionResult Gateway(string gatewayName, string? path, [FromBody] GatewayPostBody postBody) {
 			if (!Gateways.TryGetValue(gatewayName, out var gwInfo))
 				return NotFound();
-			switch (gwInfo.ResultType) {
-				case GatewayInfo.ResultTypeEnum.json:
-					return Ok(new {
-						url=gwInfo.Url
+
+			if (gwInfo.Password.Enable) {
+				if (!(postBody.Password != null && postBody.Password == gwInfo.Password.Value)) {
+					_logger.LogWarning(
+						"[Gateway Name: {gtwName}] 密码验证失败，输入：{inpPw}"
+						, gwInfo.Name
+						, postBody.Password
+						);
+					return Unauthorized(new {
+						code = 401,
+						error = "wrong password"
 					});
+				}
+			}
+
+			switch (gwInfo.ResultType) {
+				case GatewayInfo.ResultTypeEnum.json: {
+						Dictionary<string, object> resParas = [];
+						foreach(var p in gwInfo.Parameters) {
+							resParas.Add(p.Name, p.Value);
+						}
+
+						return Ok(new {
+							url = gwInfo.Url,
+							paras = resParas
+						});
+					}
 				case GatewayInfo.ResultTypeEnum.iframe: {
 						IActionResult iframeContent(string name, string url) => 
 							Content($@"
@@ -61,6 +134,9 @@ namespace MJYY_Web_server.Controllers {
 </body>
 </html>"
 							, "text/html");
+
+						if (gwInfo.Parameters.Length > 0) 
+							_logger.LogWarning("参数无法通过iframe模式返回，需更改为json模式或删除沉冗参数");
 						return iframeContent(gatewayName, gwInfo.Url);
 					}
 				default:
